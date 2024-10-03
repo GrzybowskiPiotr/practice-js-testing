7eb23e1e7440cfe3c8d7fc7d34c5824663f8fbd6
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-10-03 08:49:44 +0200

    first test

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,12 @@
-import randomNumber from './app';
+import randomNumber from "./app.js";
+
+function testArg11() {
+  const testExpects = 1;
+  if (randomNumber(1, 1) === testExpects) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(`Test liczb z zakresu 1 - 1. Wynik testu ${testArg11()}`);
