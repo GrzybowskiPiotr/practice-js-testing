29c3f562c6ff6ec3faa94fe75ee4edf7a5c3e9aa
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-10-03 10:48:43 +0200

    done

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,39 @@
 export default class User {
+  constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
+    this.emailCheck();
+    this.passwordCheck();
+  }
 
+  emailCheck() {
+    let reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    let test = reg.test(this.email);
+    if (!test) {
+      throw new Error("email is incorrect");
+    }
+  }
+
+  passwordCheck() {
+    let password = this.password;
+    if (password.split("").length < 6) {
+      throw new Error("password lenght is inccorect");
+    }
+  }
+
+  getEmail() {
+    return this.email;
+  }
+  getPassword() {
+    return this.password;
+  }
+  login() {
+    const reg = /^[\w-\.]+@devmentor.pl/g;
+    let test = reg.test(this.email);
+    if (test) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
