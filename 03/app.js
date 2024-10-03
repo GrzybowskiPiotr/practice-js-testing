2a36683da07d76af27acc908d4485bbf14f1dd7a
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-10-03 09:24:39 +0200

    test 4

--- 03/app.js
+++ 03/app.js
@@ -5,6 +5,8 @@ export default function randomNumber(min, max) {
 
   if (!isNumberic(min) || !isNumberic(max)) {
     throw new Error("Arguments must be of type Number");
+  } else if (min > max) {
+    throw new Error("The first argument must have lower value then second");
   } else {
     return Math.random() * (min - max) + min;
   }
--- 03/app.test.js
+++ 03/app.test.js
@@ -3,25 +3,51 @@ import randomNumber from "./app.js";
 function testArg11() {
   const testExpects = 1;
   if (randomNumber(1, 1) === testExpects) {
-    return true;
+    return `test Ok.`;
   } else {
-    return false;
+    return `test not ok.`;
   }
 }
 
 function testArgsAreNumbes() {
   try {
     randomNumber("a", 2);
-  } catch (error) {
-    return `Funkcja zwraca ${error}`;
+  } catch (e) {
+    if ((e = "Arguments must be of type Number")) {
+      return `Test ok. Funkcja zwraca ${e}`;
+    }
   }
 
   try {
     randomNumber(2, "D");
   } catch (e) {
-    return `Funkcja zwraca błąd ${e}`;
+    if ((e = "Arguments must be of type Number")) {
+      return `Test ok. Funkcja zwraca ${e}`;
+    }
   }
 }
 
-console.log(`Test liczb z zakresu 1 - 1. Wynik testu ${testArg11()}`);
-console.log(`Test błędnego argumentu. Wynik testu ${testArgsAreNumbes()}`);
+function testValueArgs() {
+  try {
+    randomNumber(4, 3);
+  } catch (error) {
+    if (error) return `Test ok. ${error}`;
+    else {
+      return `Test not ok. ${error}`;
+    }
+  }
+}
+
+function testRangeVal(min, max) {
+  const randomeNumber = randomNumber(min, max);
+  if (randomeNumber > min || randomeNumber < max) {
+    return `test ok. wartość mieści się pomiędzy ${min} - ${max}`;
+  } else {
+    return `Test Nok. wartość poza zakresem`;
+  }
+}
+
+console.log(`Test liczb z zakresu 1 - 1. ${testArg11()}`);
+console.log(`Test błędnego argumentu. ${testArgsAreNumbes()}`);
+console.log(`Test błędnego zakresu argumentów. ${testValueArgs()}`);
+console.log(`Test wartosci w zakresie ${testRangeVal(3, 9)}`);
